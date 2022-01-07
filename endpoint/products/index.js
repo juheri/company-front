c0b5597c779d4e8791a8154c7e0dff4d8f98b021
unknown <juheri842@gmail.com>
2022-01-07 15:13:37 +0700

    add feature detail product and delete product

--- endpoint/products/index.js
+++ endpoint/products/index.js
@@ -21,9 +21,24 @@ export const getProduct = async (token, page) => {
         url: process.env.base_url + "/product-company?page="+page+"&per_page=10",
         headers: {
             "Authorization": process.env.bearer_token,
-            "Content-Type": "multipart/form-data",
+            "Content-Type": "application/json",
             token
         }
     });
     return result
+}
+
+export const deleteProduct = async (product_id, token) => {
+    return await Axios({
+        method: "DELETE",
+        url: process.env.base_url + "/product-destroy",
+        headers: {
+            "Authorization": process.env.bearer_token,
+            "Content-Type": "application/json",
+            token
+        },
+        data: {
+            product_id
+        }
+    });
 }
